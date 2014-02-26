a43e4d0cc02ad7adbbb198d8040164ca7e7f20e9
liuzhaoguo <liu_zhaoguo@126.com>
2014-02-26 13:52:19 +0800

    make the code more standard according to the advice of HLJ

--- froi/algorithm/regiongrow.py
+++ froi/algorithm/regiongrow.py
@@ -5,33 +5,36 @@ import numpy as np
 
 def region_growing(image,coordinate,number):
     """
-    Give a coordinate ,return a region.
+    Give coordinate and size,return a region.
+
     """
+    # tmp_image store marks and rg_image store new image after region grow
     nt = number
-    tmp_image = np.zeros_like(image)     #initialize all 0 image
-    rg_image = np.zeros_like(image)     #store new image after grow
+    tmp_image = np.zeros_like(image)
+    rg_image = np.zeros_like(image)
     image_shape = image.shape
 
     x = coordinate[0]
     y = coordinate[1]
     z = coordinate[2]
 
-
-    inside = (x>=0)and(x<image_shape[0])and(y>=0)and \
-             (y<image_shape[1])and(z>=0)and(z<image_shape[2])
-    if inside!=True:
+    # ensure the coordinate is in the image
+    inside = (x >= 0) and (x < image_shape[0]) and (y >= 0) and \
+             (y < image_shape[1]) and (z >= 0) and (z < image_shape[2])
+    if inside != True:
         print "The coordinate is out of the image range."
         return False
 
-    region_mean = image[x,y,z]     #the mean value
+    # initialize region_mean and region_size
+    region_mean = image[x, y, z]
+    region_size = 0
 
-    region_size = 0              #the size of region
-    #voxel_distance = 0.0        #difference in neighbor_voxels
+    # initialize neighbor_list with 10000 rows 4 columns
+    neighbor_free = 10000
+    neighbor_pos = -1
+    neighbor_list = np.zeros((neighbor_free, 4))
 
-    neighbor_free = 10000      #longth of free space for list
-    neighbor_pos = -1          #the position of seed voxel in list
-    neighbor_list = np.zeros((neighbor_free,4))   #create a list with 10000x4
-
+    # 26 direct neighbor points
     neighbors  = [[1,0,0], \
                   [-1,0,0], \
                   [0,1,0], \
@@ -59,50 +62,52 @@ def region_growing(image,coordinate,number):
                   [-1,0,1], \
                   [-1,1,0]]
 
-    while region_size < nt:       # the size of region < nt
-
-        for i in range(6):        #do it for 6 times
+    while region_size < nt:
+        # (xn, yn, zn) store direct neighbor of seed point
+        for i in range(6):
             xn = x + neighbors[i][0]
             yn = y + neighbors[i][1]
             zn = z + neighbors[i][2]
 
-            inside = (xn>=0)and(xn<image_shape[0])and(yn>=0)and(yn<image_shape[1])and(zn>=0)and(zn<image_shape[2])       #coordinate is not out of range
+            # ensure the coordinate is in the image
+            inside = (xn >= 0) and (xn < image_shape[0]) and (yn >= 0) and \
+                     (yn < image_shape[1]) and (zn >= 0) and (zn < image_shape[2])
 
+            # ensure the original flag 0 is not changed
+            if inside and tmp_image[xn, yn, zn]==0:
+                # add this point to neighbor_list and mark it with 1
+                neighbor_pos = neighbor_pos + 1
+                neighbor_list[neighbor_pos] = [xn, yn, zn, image[xn, yn, zn]]
+                tmp_image[xn, yn, zn] = 1
 
-            if inside and tmp_image[xn,yn,zn]==0:     #the original flag 0 is not changed
-                neighbor_pos = neighbor_pos+1        #the position of new voxel in list
-                neighbor_list[neighbor_pos] = [xn,yn,zn,image[xn,yn,zn]]   #put (x,y,z,value) into 4-dimensions list
-                tmp_image[xn,yn,zn] = 1            #voxel in 4-dimensions list with flag 1
+        # ensure there is enough space to store neighbor_list
+        if (neighbor_pos+100 > neighbor_free):
+            neighbor_free += 10000
+            new_list = np.zeros((10000, 4))
+            neighbor_list = np.vstack((neighbor_list, new_list))
 
+        # the distance between every neighbor point value to new region mean value
+        distance = np.abs(neighbor_list[:neighbor_pos+1, 3] - np.tile(region_mean, neighbor_pos+1))
 
+        # chose the min distance point
+        #voxel_distance = distance.min()
+        index = distance.argmin()
 
-        if (neighbor_pos+100 > neighbor_free):   #if the longth of list is not enough
-            neighbor_free +=10000                #add another 10000
-            new_list = np.zeros((10000,4))
+        # mark the new region point with 2 and update new image
+        tmp_image[x, y, z] = 2
+        rg_image[x, y, z] = image[x, y, z]
+        region_size += 1
 
-            neighbor_list = np.vstack((neighbor_list,new_list))   #connect the two lists
-
-
-        distance = np.abs(neighbor_list[:neighbor_pos+1,3] - np.tile(region_mean,neighbor_pos+1))
-        #n+1 neighbor_voxels' value - n+1 mean values
-
-        #voxel_distance = distance.min() #choose the min difference voxel
-        index = distance.argmin()    #the index of min difference voxel
-
-
-        tmp_image[x,y,z] = 2   #segmental region with flag 2
-        rg_image[x,y,z] = image[x,y,z]
-        region_size += 1      #increase the region_size
-
-        x = neighbor_list[index][0]   #
+        # (x, y, z) the new seed point
+        x = neighbor_list[index][0]
         y = neighbor_list[index][1]
         z = neighbor_list[index][2]
 
-        region_mean = (region_mean*region_size+neighbor_list[index,3])/(region_size+1)   #refresh the mean value
+        # update region mean value
+        region_mean = (region_mean*region_size+neighbor_list[index, 3])/(region_size+1)
 
-
-        neighbor_list[index] = neighbor_list[neighbor_pos]   #the head of list ---> the min difference voxel in the list
-        neighbor_pos -= 1  #the number of voxels in the neighbor_list decrease 1
-
+        # remove the seed point from neighbor_list
+        neighbor_list[index] = neighbor_list[neighbor_pos]
+        neighbor_pos -= 1
 
     return rg_image
